ef39826afacab0bac2863d1c2fb62f0b19990c60
proforty7 <proforty7@gmail.com>
2020-08-12 02:28:21 +0530

    add hero content
    Signed-off-by: proforty7 <proforty7@gmail.com>

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -4,7 +4,7 @@ import classes from "./index.module.scss";
 const Navbar = () => {
   return (
     <nav className={[classes.nav, classes.light].join(" ")}>
-      <div className={classes.navContainer}>
+      <div className={[classes.navContainer, classes.container].join(" ")}>
         <div className={classes.logo}>roads'nrumble</div>
         <div className={classes.spacer}></div>
         <div className={classes.navItem}>contact</div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,30 @@ import React from "react";
 import classes from "./index.module.scss";
 
 const IndexPage = () => {
-  return <div className={classes.screen}></div>;
+  return (
+    <div className={classes.screen}>
+      <div className={[classes.heroContent, classes.container].join(" ")}>
+        <div className={classes.column}>
+          <h1>Roads 'N Rumbles</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            excepturi minus aperiam deleniti facilis voluptates quo velit?
+            Eligendi, consectetur consequatur?
+          </p>
+          <div className={classes.btnContainer}>
+            <button className={[classes.btn, classes.heroBtn].join(" ")}>
+              Get Started
+            </button>
+            <button
+              className={[classes.btn, classes.flat, classes.heroBtn].join(" ")}
+            >
+              Shop Accessories
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default IndexPage;
